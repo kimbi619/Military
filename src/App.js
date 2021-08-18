29067e7f074126df1361e0a0e251a8e26060dd3d
unknown <kimbidarl@gmail.com>
2021-08-18 03:36:20 +0100

    first deploy of military

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        <h2>This is a recreation of the MILITARY website from school</h2>
+       <p>this shit is causing me a lot of problems</p>
       </header>
     </div>
   );
